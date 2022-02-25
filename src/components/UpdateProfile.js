756f7fc6f5cc1569f508840700a6227586f5c578
Bhanu Pratap Singh Rathore <81702392+BPSR23@users.noreply.github.com>
2022-02-25 23:44:23 +0530

    commit

---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,114 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+// import DatePicker from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+
+export default function UpdateProfile() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const nameRef = useRef()
+  const addressRef = useRef()
+  const { currentUser, updatePassword, updateEmail } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+//   const [date, setDate] = useState(new Date())
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Passwords do not match")
+    }
+
+    const promises = []
+    setLoading(true)
+    setError("")
+
+    if (emailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(emailRef.current.value))
+    }
+    if (passwordRef.current.value) {
+      promises.push(updatePassword(passwordRef.current.value))
+    }
+
+    Promise.all(promises)
+      .then(() => {
+        history.push("/")
+      })
+      .catch(() => {
+        setError("Failed to update account")
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Update Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+          <Form.Group id="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                ref={nameRef}
+                required
+                defaultValue={currentUser.name}
+              />
+            </Form.Group>
+            {/* <Form.Group id="dob">
+            <Form.Label>DOB</Form.Label>
+            <DatePicker selected={date} onChange={date => setDate(date)} />
+            </Form.Group> */}
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                required
+                defaultValue={currentUser.email}
+              />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                placeholder="Leave blank to keep the same"
+              />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                placeholder="Leave blank to keep the same"
+              />
+            </Form.Group>
+            <Form.Group id="address">
+              <Form.Label>LeetCode</Form.Label>
+              <Form.Control
+                type="address"
+                ref={addressRef}
+                required
+                defaultValue={currentUser.address}
+              />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Update
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Link to="/">Cancel</Link>
+      </div>
+    </>
+  )
+}
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,77 @@
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateName(name) {
+    return currentUser.updateName(name);
+  }
+
+  function updateDob(dob) {
+    return currentUser.updateDob(dob);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
+  function updateAddress(address) {
+    return currentUser.updateAddress(address);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateName,
+    updateDob,
+    updateEmail,
+    updatePassword,
+    updateAddress,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
